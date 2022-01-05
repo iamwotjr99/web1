da1eb4a90027a54fefb389114ea78a8a42d9d1b3
iamwotjr99 <iamwotjr@gmail.com>
2022-01-05 22:51:15 +0900

    Controller, main, storage

---
+++ react-lecture/1-vanilla/js/Controller.js
@@ -0,0 +1,36 @@
+import { emit, on } from "../js/helpers";
+
+const tag  = "[View]";
+
+export default class View {
+    constructor(element) {
+        console.log(tag, "constuctor");
+
+        if(!element) throw "no element";
+
+        this.element = element;
+        this.originalDisplay = this.element.style.display || "";
+
+        return this;
+    }
+
+    hide() {
+        this.element.style.display = "none";
+        return this;
+    }
+
+    show() {
+        this.element.style.display = this.originalDisplay;
+        return this;
+    }
+
+    on(eventName, handler) {
+        on(this.element, eventName, handler);
+        return this;
+    }
+
+    emit(eventName, data) {
+        emit(this.element, eventName, data);
+        return this;
+    }
+}
